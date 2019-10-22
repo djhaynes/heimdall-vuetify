a975eac1a6c63507cb4547d4adf434995027710e
Danny Haynes <dhaynes@mitre.org>
2019-10-22 09:27:02 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,10 @@ const repository = parsed.repository.url || "";
 const license = parsed.license || "";
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  #publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  publicPath: process.env.NODE_ENV === "production"
+  ? "heimdall-vuetify"
+  : "/"
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
